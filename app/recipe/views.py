5c0631451ac56dd181a8723f01c7592bcdb4c9ee
Muhammad Ata-ur-rehman <ataurrehman56@gmail.com>
2020-06-03 10:36:48 +0500

    Added ingredients list

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import mixins, viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from core.models import Tag
+from core.models import Ingredient, Tag
 from recipe import serializers
 
 
@@ -14,6 +14,23 @@ class TagViewSet(viewsets.GenericViewSet,
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     queryset = Tag.objects.all()
+
+    def get_queryset(self):
+        """Return objects for current authenticated user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """ Manage Ingredient in database """
+    serializer_class = serializers.IngredientSerializer
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+    queryset = Ingredient.objects.all()
 
     def get_queryset(self):
         """Return objects for current authenticated user only"""
